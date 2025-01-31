f856d2165bf32c25f006519b34f187cfe305118b
Owen Buckley <owenbuckley13@gmail.com>
2025-01-31 12:06:46 -0500

    wip

--- infra/api-routes.ts
+++ infra/api-routes.ts
@@ -6,11 +6,17 @@ export const api = new sst.aws.ApiGatewayV2("Api");
 // TODO need to handle basePath here?  (and / or all adapters?)
 
 api.route("GET /api/hello", {
-  // link: [bucket],
   handler: "functions/hello.handler",
 });
 
+// api.route("GET /api/fragment", {
+//   handler: ".aws/api/fragment/index.handler",
+// });
+
 api.route("GET /api/greeting", {
-  // link: [bucket],
   handler: ".aws/api/greeting/index.handler",
+});
+
+api.route("POST /api/search", {
+  handler: ".aws/api/search/index.handler",
 });
