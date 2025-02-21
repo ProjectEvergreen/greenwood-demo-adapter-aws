c8e2f59ff52ef59b7ee0e7d2e4fa7eda319c5a1e
Owen Buckley <owenbuckley13@gmail.com>
2025-02-20 20:55:55 -0500

    configure runtime

--- infra/api-routes.ts
+++ infra/api-routes.ts
@@ -1,5 +1,7 @@
 export const api = new sst.aws.ApiGatewayV2("Api");
 
+const RUNTIME = "nodejs22.x";
+
 // TODO need to handle basePath here?  (and / or all adapters?)
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
 const apiRoutes = ((await import(new URL('../../public/manifest.json', import.meta.url), { with: { type: 'json' } })).default).apis.value;
@@ -14,7 +16,7 @@ ssrPages.forEach((page) => {
   api.route(`GET /routes/${id}`, {
     bundle: `.aws-output/routes/${id}`,
     handler: "index.handler",
-    // runtime: "nodejs22.x"
+    runtime: RUNTIME
   });
 })
 
@@ -24,6 +26,6 @@ apiRoutes.forEach((apiRoute) => {
   api.route(`ANY ${route}`, {
     bundle: `.aws-output/${route}`,
     handler: "index.handler",
-    // runtime: "nodejs22.x"
+    runtime: RUNTIME
   })
 });
