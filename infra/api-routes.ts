838fe155bb7e7dab3987d3da5692c1d9496daf36
Owen Buckley <owenbuckley13@gmail.com>
2025-07-31 09:08:50 -0400

    refactor API routes configuration

--- infra/api-routes.ts
+++ infra/api-routes.ts
@@ -4,28 +4,28 @@ const RUNTIME = "nodejs22.x";
 
 // TODO need to handle basePath here?  (and / or all adapters?)
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
-// const apiRoutes = ((await import(new URL('../../public/manifest.json', import.meta.url), { with: { type: 'json' } })).default).apis.value;
+const apiRoutes = ((await import(new URL('../../public/manifest.json', import.meta.url), { with: { type: 'json' } })).default).apis.value;
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
 const ssrPages = ((await import(new URL('../../public/graph.json', import.meta.url), { with: { type: 'json' } })).default).filter(page => page.isSSR);
 
 // https://sst.dev/docs/component/aws/apigatewayv2
 // https://sst.dev/docs/component/aws/function
 ssrPages.forEach((page) => {
-  const { id, route } = page;
+  const { id } = page;
 
-  api.route(`GET /routes${route}`, {
+  api.route(`GET /routes/${id}`, {
     bundle: `.aws-output/routes/${id}`,
     handler: "index.handler",
     runtime: RUNTIME
   });
 })
 
-// apiRoutes.forEach((apiRoute) => {
-//   const [route, details] = apiRoute;
+apiRoutes.forEach((apiRoute) => {
+  const [route] = apiRoute;
 
-//   api.route(`ANY ${route}`, {
-//     bundle: `.aws-output/api/${details.id}`,
-//     handler: "index.handler",
-//     runtime: RUNTIME
-//   })
-// });
+  api.route(`ANY ${route}`, {
+    bundle: `.aws-output/${route}`,
+    handler: "index.handler",
+    runtime: RUNTIME
+  })
+});
