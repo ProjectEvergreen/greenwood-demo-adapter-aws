d3fac8bac565e794bc4523f486cf9d6166d7df36
Owen Buckley <owenbuckley13@gmail.com>
2025-11-29 14:45:31 -0500

    back to dynamic suffix

--- infra/router.ts
+++ infra/router.ts
@@ -18,7 +18,7 @@ ssrPages.forEach((page) => {
     console.log(` - route: ${regexRoute} -> /routes/${id}`);
     console.log({ route, id, routeKey });
 
-    ssrRoutes[routeKey] = {
+    ssrRoutes['/product/'] = {
       url: api.url,
       rewrite: {
         // TODO: un-hardcode
