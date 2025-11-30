06e48fa24fb7f0106992ab51054e6cbc6fa22537
Owen Buckley <owenbuckley13@gmail.com>
2025-11-29 20:16:44 -0500

    forward path patch handling in adapter

--- infra/router.ts
+++ infra/router.ts
@@ -22,8 +22,11 @@ ssrPages.forEach((page) => {
       url: api.url,
       rewrite: {
         // TODO: un-hardcode
+        // TODO: we're dropping the tailing / slash here, e.g. only `/product/1` is matching
         regex: "^/product/(.*)$",
         to: "/routes/product/$1"
+        // regex: "^/product/?(.*)$",
+        // to: "/routes/product/$1/",
         // regex: `^${regexRoute}$`,
         // to: '/route/product/1/'
         // to: `/routes/${id}`
