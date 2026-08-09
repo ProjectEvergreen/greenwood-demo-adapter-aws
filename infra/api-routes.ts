b3a77b738be7efc33f695b715678906cdf826b45
Owen Buckley <owenbuckley13@gmail.com>
2026-08-08 20:21:28 -0400

    enhancement: upgrade greenwood v0.35.0

--- infra/api-routes.ts
+++ infra/api-routes.ts
@@ -1,6 +1,6 @@
 export const api = new sst.aws.ApiGatewayV2("Api");
 
-const RUNTIME = "nodejs22.x";
+const RUNTIME = "nodejs24.x";
 
 // TODO need to handle basePath here?  (and / or all adapters?)
 // @ts-expect-error see https://github.com/microsoft/TypeScript/issues/42866
